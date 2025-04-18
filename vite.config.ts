8c0f3584a0fe16ef341ffaecf033928193364f6b
CC11001100 <CC11001100@users.noreply.github.com>
2025-04-18 11:53:38 +0000

    deploy: e7edffabb5541c454319ce80e16203ed620580af

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/fuck-algorithm/leetcode-448-find-all-numbers-disappeared-in-an-array/',
-})
