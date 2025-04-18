b05e8913118be0bc39353809a93b081c06f8a8ca
CC11001100 <CC11001100@qq.com>
2025-04-18 19:39:53 +0800

    feat: 完善D3模拟实现，支持更多SVG元素和动画效果

--- src/utils/d3Mock.ts
+++ src/utils/d3Mock.ts
@@ -2,35 +2,17 @@
  * 简化版D3模拟实现，用于绘制动画
  */
 
-// 元素类型
-export type D3Selection = SVGSelection | TextSelection | RectSelection | PathSelection | GroupSelection;
-
-// SVG选择器
-export function select(element: SVGSVGElement): SVGSelection {
-  return new SVGSelection(element);
+// 基础D3选择器接口
+export interface D3Selection {
+  attr(name: string, value: string | number): D3Selection;
+  text(content: string): D3Selection;
+  append(elementType: string): D3Selection;
+  selectAll(selector: string): D3Selection;
+  remove(): D3Selection;
 }
 
-// 动画缓动函数
-export const easeLinear = (t: number) => t;
-export const easeQuadOut = (t: number) => t * (2 - t);
-export const easeBackOut = (t: number) => {
-  const c1 = 1.70158;
-  const c3 = c1 + 1;
-  return 1 + c3 * Math.pow(t - 1, 3) + c1 * Math.pow(t - 1, 2);
-};
-export const easeElasticOut = (t: number) => {
-  const c4 = (2 * Math.PI) / 3;
-  return t === 0
-    ? 0
-    : t === 1
-      ? 1
-      : Math.pow(2, -10 * t) * Math.sin((t * 10 - 0.75) * c4) + 1;
-};
-export const easePolyIn = (t: number) => Math.pow(t, 3);
-export const easeExpOut = (t: number) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t));
-
 // SVG选择器实现
-class SVGSelection {
+class SVGSelection implements D3Selection {
   element: SVGSVGElement;
   private currentGroup: SVGGElement | null = null;
 
@@ -59,6 +41,12 @@ class SVGSelection {
   // 设置属性
   attr(name: string, value: string | number): SVGSelection {
     this.element.setAttribute(name, value.toString());
+    return this;
+  }
+
+  // 设置文本内容
+  text(content: string): SVGSelection {
+    this.element.textContent = content;
     return this;
   }
 
@@ -100,7 +88,7 @@ class SVGSelection {
 }
 
 // 文本元素选择器
-class TextSelection {
+class TextSelection implements D3Selection {
   element: SVGTextElement;
 
   constructor(element: SVGTextElement) {
@@ -108,17 +96,7 @@ class TextSelection {
   }
 
   attr(name: string, value: string | number): TextSelection {
-    if (name === 'class') {
-      this.element.setAttribute('class', value.toString());
-    } else if (name === 'x') {
-      this.element.setAttribute('x', value.toString());
-    } else if (name === 'y') {
-      this.element.setAttribute('y', value.toString());
-    } else if (name === 'text-anchor') {
-      this.element.setAttribute('text-anchor', value.toString());
-    } else {
-      this.element.setAttribute(name, value.toString());
-    }
+    this.element.setAttribute(name, value.toString());
     return this;
   }
 
@@ -126,10 +104,25 @@ class TextSelection {
     this.element.textContent = content;
     return this;
   }
+
+  append(_elementType: string): D3Selection {
+    throw new Error('Text elements cannot append children');
+  }
+
+  selectAll(_selector: string): D3Selection {
+    throw new Error('Text elements do not support selectAll');
+  }
+
+  remove(): D3Selection {
+    if (this.element.parentNode) {
+      this.element.parentNode.removeChild(this.element);
+    }
+    return this;
+  }
 }
 
 // 矩形元素选择器
-class RectSelection {
+class RectSelection implements D3Selection {
   element: SVGRectElement;
 
   constructor(element: SVGRectElement) {
@@ -137,27 +130,32 @@ class RectSelection {
   }
 
   attr(name: string, value: string | number): RectSelection {
-    if (name === 'class') {
-      this.element.setAttribute('class', value.toString());
-    } else if (name === 'x') {
-      this.element.setAttribute('x', value.toString());
-    } else if (name === 'y') {
-      this.element.setAttribute('y', value.toString());
-    } else if (name === 'width') {
-      this.element.setAttribute('width', value.toString());
-    } else if (name === 'height') {
-      this.element.setAttribute('height', value.toString());
-    } else if (name === 'rx') {
-      this.element.setAttribute('rx', value.toString());
-    } else {
-      this.element.setAttribute(name, value.toString());
+    this.element.setAttribute(name, value.toString());
+    return this;
+  }
+
+  text(content: string): RectSelection {
+    throw new Error('Rect elements do not support text content');
+  }
+
+  append(_elementType: string): D3Selection {
+    throw new Error('Rect elements cannot append children');
+  }
+
+  selectAll(_selector: string): D3Selection {
+    throw new Error('Rect elements do not support selectAll');
+  }
+
+  remove(): D3Selection {
+    if (this.element.parentNode) {
+      this.element.parentNode.removeChild(this.element);
     }
     return this;
   }
 }
 
 // 路径元素选择器
-class PathSelection {
+class PathSelection implements D3Selection {
   element: SVGPathElement;
 
   constructor(element: SVGPathElement) {
@@ -165,21 +163,32 @@ class PathSelection {
   }
 
   attr(name: string, value: string | number): PathSelection {
-    if (name === 'class') {
-      this.element.setAttribute('class', value.toString());
-    } else if (name === 'd') {
-      this.element.setAttribute('d', value.toString());
-    } else if (name === 'fill') {
-      this.element.setAttribute('fill', value.toString());
-    } else {
-      this.element.setAttribute(name, value.toString());
+    this.element.setAttribute(name, value.toString());
+    return this;
+  }
+
+  text(content: string): PathSelection {
+    throw new Error('Path elements do not support text content');
+  }
+
+  append(_elementType: string): D3Selection {
+    throw new Error('Path elements cannot append children');
+  }
+
+  selectAll(_selector: string): D3Selection {
+    throw new Error('Path elements do not support selectAll');
+  }
+
+  remove(): D3Selection {
+    if (this.element.parentNode) {
+      this.element.parentNode.removeChild(this.element);
     }
     return this;
   }
 }
 
 // 组元素选择器
-class GroupSelection {
+class GroupSelection implements D3Selection {
   element: SVGGElement;
 
   constructor(element: SVGGElement) {
@@ -187,12 +196,12 @@ class GroupSelection {
   }
 
   attr(name: string, value: string | number): GroupSelection {
-    if (name === 'transform') {
-      this.element.setAttribute('transform', value.toString());
-    } else {
-      this.element.setAttribute(name, value.toString());
-    }
+    this.element.setAttribute(name, value.toString());
     return this;
+  }
+
+  text(content: string): GroupSelection {
+    throw new Error('Group elements do not support direct text content');
   }
 
   append(elementType: string): D3Selection {
@@ -212,4 +221,39 @@ class GroupSelection {
     
     return this;
   }
-} 
+
+  selectAll(_selector: string): D3Selection {
+    throw new Error('Group elements do not support selectAll');
+  }
+
+  remove(): D3Selection {
+    if (this.element.parentNode) {
+      this.element.parentNode.removeChild(this.element);
+    }
+    return this;
+  }
+}
+
+// SVG选择器工厂函数
+export function select(element: SVGSVGElement): SVGSelection {
+  return new SVGSelection(element);
+}
+
+// 动画缓动函数
+export const easeLinear = (t: number) => t;
+export const easeQuadOut = (t: number) => t * (2 - t);
+export const easeBackOut = (t: number) => {
+  const c1 = 1.70158;
+  const c3 = c1 + 1;
+  return 1 + c3 * Math.pow(t - 1, 3) + c1 * Math.pow(t - 1, 2);
+};
+export const easeElasticOut = (t: number) => {
+  const c4 = (2 * Math.PI) / 3;
+  return t === 0
+    ? 0
+    : t === 1
+      ? 1
+      : Math.pow(2, -10 * t) * Math.sin((t * 10 - 0.75) * c4) + 1;
+};
+export const easePolyIn = (t: number) => Math.pow(t, 3);
+export const easeExpOut = (t: number) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)); 
