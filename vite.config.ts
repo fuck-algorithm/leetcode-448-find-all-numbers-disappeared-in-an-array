bd38fa4555479ac24537a75e73dbbc91cb6074d1
CC11001100 <CC11001100@qq.com>
2025-04-18 19:40:16 +0800

    chore: 配置GitHub Pages自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/fuck-algorithm/leetcode-448-find-all-numbers-disappeared-in-an-array/',
 })
